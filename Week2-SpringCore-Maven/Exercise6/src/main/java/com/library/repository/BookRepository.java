ece8b4aaee66a63a89a29164f31a780f65e59ed2
Varshith Krishna Mohan Reddy Tummuru <varshithreddy6776@gmail.com>
2026-07-08 09:53:38 +0530

    Completed Week 2 Spring Core Maven Exercise 6 - Annotation Based Beans

---
+++ Week2-SpringCore-Maven/Exercise6/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+
+    public void displayBooks() {
+        System.out.println("BookRepository: Fetching books using annotation configuration.");
+    }
+}
---
+++ Week2-SpringCore-Maven/Exercise6/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,22 @@
+package com.library.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.library.repository.BookRepository;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBooks() {
+        System.out.println("BookService: Calling repository using annotation configuration.");
+        bookRepository.displayBooks();
+    }
+}
