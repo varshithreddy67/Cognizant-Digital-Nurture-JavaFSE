3af1569a7b8fb35e5f231f28d5c5be87c8ad4111
Varshith Krishna Mohan Reddy Tummuru <varshithreddy6776@gmail.com>
2026-07-08 09:36:45 +0530

    Completed Week 2 Spring Core Maven Exercise 4 - Maven Project Configuration

---
+++ Week2-SpringCore-Maven/Exercise4/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,10 @@
+package com.library;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Library Management Maven Project Created Successfully.");
+
+    }
+}
