ff3ebc8812d57d7fae96e4dd69d0f403f2de34ad
Varshith Krishna Mohan Reddy Tummuru <varshithreddy6776@gmail.com>
2026-07-22 14:18:38 +0530

    Completed Exercise 8 - Angular Unit Testing

--- Week5-Angular/student-course-portal/src/app/course/course.spec.ts
+++ Week5-Angular/student-course-portal/src/app/course/course.spec.ts
@@ -1,23 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Course } from './course';
+import { CourseService } from '../services/course.service';
+import { of } from 'rxjs';
 
 describe('Course', () => {
+
   let component: Course;
   let fixture: ComponentFixture<Course>;
 
+  let courseServiceMock = {
+    getCourses: () => of([
+      {
+        id: 1,
+        name: "Java"
+      },
+      {
+        id: 2,
+        name: "Angular"
+      }
+    ])
+  };
+
+
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      imports: [Course]
-    })
-    .compileComponents();
+
+      imports: [Course],
+
+      providers: [
+        {
+          provide: CourseService,
+          useValue: courseServiceMock
+        }
+      ]
+
+    }).compileComponents();
+
 
     fixture = TestBed.createComponent(Course);
+
     component = fixture.componentInstance;
+
     fixture.detectChanges();
+
   });
 
-  it('should create', () => {
+
+  it('should create course component', () => {
+
     expect(component).toBeTruthy();
+
   });
+
+
+  it('should load courses', () => {
+
+    expect(component.courses.length)
+      .toBe(2);
+
+  });
+
+
 });
